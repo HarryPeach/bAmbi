619f0b9d6456207e776cc3873d206db210f484e7
Harry Peach (Personal Signing Key) <harrypeach1999@gmail.com>
2021-03-22 23:19:41 +0000

    Fix orientation issues with average colour transformer

--- bambi/transformers/average_colour_transformer.py
+++ bambi/transformers/average_colour_transformer.py
@@ -20,29 +20,31 @@ class AverageColourTransformer(BaseTransformer):
         bounding_box_height = im.height / (layout.dimensions[1] + 2)
 
         for i in range(len(layout.top_state)):
-            start_x = (i + 1) * bounding_box_width
+            # ir reverses the order of the colours
+            ir = len(layout.top_state) - 1 - i
+            start_x = (ir + 1) * bounding_box_width
             start_y = 0
             layout.top_state[i] = self._draw_box(im, bounding_box_width,
                                                  bounding_box_height, start_x, start_y)
 
         for i in range(len(layout.right_state)):
+            # ir reverses the order of the colours
+            ir = len(layout.right_state) - 1 - i
             start_x = im.width - bounding_box_width
-            start_y = (i + 1) * bounding_box_height
+            start_y = (ir + 1) * bounding_box_height
             layout.right_state[i] = self._draw_box(im, bounding_box_width,
                                                    bounding_box_height, start_x, start_y)
 
         for i in range(len(layout.bottom_state)):
-            # ir reverses the order of the colours
-            ir = len(layout.bottom_state) - 1 - i
-            start_x = (ir + 1) * bounding_box_width
+            start_x = (i + 1) * bounding_box_width
             start_y = im.height - bounding_box_height
             layout.bottom_state[i] = self._draw_box(im, bounding_box_width,
                                                     bounding_box_height, start_x, start_y)
 
         for i in range(len(layout.left_state)):
             # ir reverses the order of the colours
-            ir = len(layout.left_state) - 1 - i
+            # ir = len(layout.left_state) - 1 - i
             start_x = 0
-            start_y = (ir + 1) * bounding_box_height
+            start_y = (i + 1) * bounding_box_height
             layout.left_state[i] = self._draw_box(im, bounding_box_width,
                                                   bounding_box_height, start_x, start_y)
